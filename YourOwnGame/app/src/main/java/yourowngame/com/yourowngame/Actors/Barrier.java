1606143cefe441fb9d2650d987fee065319839c7
Android <crisjonny@live.at>
2018-02-14 17:50:58 +0100

    Grundgeruest, Vorschlaege was besser machen!

---
+++ YourOwnGame/app/src/main/java/yourowngame/com/yourowngame/Actors/Barrier.java
@@ -0,0 +1,16 @@
+package yourowngame.com.yourowngame.Actors;
+
+/**
+ * Created by Solution on 14.02.2018.
+ */
+
+public class Barrier extends GameObject {
+    public Barrier(double posX, double posY, double speedX, double speedY) {
+        super(posX, posY, speedX, speedY);
+    }
+
+    @Override
+    protected void update() {
+
+    }
+}
---
+++ YourOwnGame/app/src/main/java/yourowngame/com/yourowngame/Actors/Player.java
@@ -0,0 +1,18 @@
+package yourowngame.com.yourowngame.Actors;
+
+/**
+ * Created by Solution on 14.02.2018.
+ */
+
+public class Player extends GameObject {
+
+
+    public Player(double posX, double posY, double speedX, double speedY) {
+        super(posX, posY, speedX, speedY);
+    }
+
+    @Override
+    protected void update() {
+
+    }
+}
