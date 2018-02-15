01296f42eaefc011fde10edd5590ef5d118b4634
Katharina Leitner <crisjonny@live.at>
2018-02-15 14:06:07 +0100

    bugfix

--- YourOwnGame/app/src/main/java/yourowngame/com/yourowngame/classes/actors/GameObject.java
+++ YourOwnGame/app/src/main/java/yourowngame/com/yourowngame/classes/actors/GameObject.java
@@ -1,6 +1,7 @@
 package yourowngame.com.yourowngame.classes.actors;
 
 import android.app.Activity;
+import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
@@ -16,13 +17,15 @@ public abstract class GameObject extends View {
 
     protected double posX, posY, speedX, speedY;
     protected String name;
+    protected Image img;
+
     //add, add, add
 
     public GameObject(Activity activity) {
         super(activity);
     }
 
-    public GameObject(@NonNull Activity activity, double posX, double posY, double speedX, double speedY, @Nullable String name){
+    public GameObject(@NonNull Activity activity, double posX, double posY, double speedX, double speedY, Image img, @Nullable String name){
         super(activity);
         this.setPosX(posX);
         this.setPosY(posY);
