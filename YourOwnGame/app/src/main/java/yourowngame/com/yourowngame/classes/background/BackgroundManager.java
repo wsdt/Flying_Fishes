c9c84f07b2eb7cb919f734aea33374ab5324357b
Solution49 <crisjonny@live.at>
2018-02-21 11:30:50 +0100

    idea background

--- YourOwnGame/app/src/main/java/yourowngame/com/yourowngame/classes/background/BackgroundManager.java
+++ YourOwnGame/app/src/main/java/yourowngame/com/yourowngame/classes/background/BackgroundManager.java
@@ -5,18 +5,40 @@ import android.support.annotation.NonNull;
 import java.util.ArrayList;
 
 import yourowngame.com.yourowngame.R;
-import yourowngame.com.yourowngame.classes.background.layers.BgLayer_1_Clouds;
+import yourowngame.com.yourowngame.classes.background.layers.BgLayerClouds1;
 import yourowngame.com.yourowngame.classes.configuration.Constants;
 import yourowngame.com.yourowngame.gameEngine.GameView;
 
 /**
  * Created on 18.02.2018.
- *
  * Provides a BackgroundManager
- *
  * Singleton, always just one BackgroundManager
  *
- * TODO: exception handling, improving performance,
+ * TODO: So we've got two options now how to show our GUI Content
+ *
+ * Option 1:
+ *
+ * We somehow manage to create an animation that draws one image after another
+ * (fe -> cloud_1 has reached end of screen, cloud_2 will be drawn exactly
+ * after cloud_1 has left the screen on the right side!)
+ *
+ * Option 2:
+ *
+ * We'll make the background blue (for heaven, red for hell and so on...) and build clouds (for heaven)
+ * that will appear randomly! (which also means, that we're creating
+ * a kind of unique-UI, because the clouds will never appear
+ * in the same way, they'll always be somehow unique!
+ * main question here is, will it look as good as if we draw it with Photoshop?
+ *
+ * My Opinion:
+ *
+ * number one will surely be more interesting, because the ability of photoshop
+ * will take us further than creating it on our own.
+ * BUT -> creating unique backgrounds is kinda surprising & will surely
+ * convey something to the user. So every start on level One, will show
+ * 'em other clouds on other positions.
+ *
+ * I'll leave it to you!
  *
  */
 
@@ -38,7 +60,7 @@ public class BackgroundManager {
         this.setBackgroundLayers(new ArrayList<Background>());
         this.setGameView(gameView);
 
-        this.getBackgroundLayers().add(new BgLayer_1_Clouds(gameView, new int[] {R.drawable.bglayer1_clouds_1, R.drawable.bglayer1_clouds_2, R.drawable.bglayer1_clouds_3}, "Heaven", Constants.Background.defaultBgSpeed));
+        this.getBackgroundLayers().add(new BgLayerClouds1(gameView, new int[] {R.drawable.bglayer1_clouds_1, R.drawable.bglayer1_clouds_2, R.drawable.bglayer1_clouds_3}, "Heaven", Constants.Background.defaultBgSpeed));
         //TODO: PS: Geile Idee in Schichten zu unterteilen! (also Wolkenschicht, Landschaft usw. zu separieren)
         //TODO: Wenn du ein Levelsystem gemeint hast (für Hintergrundwechsel ab Punkteanzahl z.B., dann können wir später ja mit extends Background eigene Layerschichten oder so denk ich realisieren
             //TODO --> so auch dann levelbezogene Methoden möglich
@@ -46,7 +68,7 @@ public class BackgroundManager {
 
     public void updateAllBackgroundLayers() {
         for (Background backgroundLayer : this.getBackgroundLayers()) {
-            backgroundLayer.updateBackground(null); //todo: maybe give custom speed (maybe also by method itself or save it into background instance [= best I would say]
+            backgroundLayer.updateBackground(10); //todo: maybe give custom speed (maybe also by method itself or save it into background instance [= best I would say]
         }
     }
 
