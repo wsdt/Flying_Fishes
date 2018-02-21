d067d625c0d827b1e7187f1ca68a7320a51a0338
kevin <kevin.riedl.privat@gmail.com>
2018-02-21 22:42:29 +0100

    fuck this

--- YourOwnGame/app/src/main/java/yourowngame/com/yourowngame/classes/background/Background.java
+++ YourOwnGame/app/src/main/java/yourowngame/com/yourowngame/classes/background/Background.java
@@ -4,8 +4,13 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.Display;
+
+import java.util.Random;
 
 import yourowngame.com.yourowngame.classes.configuration.Constants;
 import yourowngame.com.yourowngame.gameEngine.GameView;
@@ -20,26 +25,37 @@ import yourowngame.com.yourowngame.gameEngine.GameView;
 
 public abstract class Background {
     private static final String TAG = "Background";
-    private GameView gameView;
     private int[] img;
     private String name;
-    private int speedX = 0;
-    private double x;
-    private double y;
-    private int activeDrawable = 0; /** image from the int array which is visible*/
+    private int backgroundSpeedX = 0;
+    private BackgroundManager backgroundManager; //contains also GameView!!
+    private Random random;
 
-    public Background(GameView gameView, int[] img, String name, int backgroundSpeed) {
+    public Background(@NonNull BackgroundManager backgroundManager, int[] img, String name, int backgroundSpeed) {
         Log.d(TAG, "getBackgroundInstance: Created new instance.");
         this.setImg(img);
         this.setName(name);
         this.setBackgroundSpeed(backgroundSpeed);
+        this.setBackgroundManager(backgroundManager);
+        this.setRandom(new Random()); //for random height/y of clouds etc.
     }
 
-    public Bitmap getCraftedBitmap(Context activityContext) {
-        //maybe later more things (like animations and so on)
-        return BitmapFactory.decodeResource(activityContext.getResources(),
-                this.getImg()[this.getActiveDrawable()]);
+    public int getRandomYforSkyElements() {
+        //uses as maximum the gameView height-50 and minimum +50!
+        int randomY = 50; //default, if view was not measured yet
+        if (this.getBackgroundManager().getGameViewHeight() != 0) {
+            randomY = random.nextInt((int) (this.getBackgroundManager().getGameViewHeight() - 50) - 50) + 50; //r.nextInt(High-Low) + Low;
+        } else {
+            Log.w(TAG, "getRandomYforSkyElements: Could not determine gameView height! Returned default random height: "+randomY);
+        }
+        return randomY;
     }
+
+    /*public Bitmap getCraftedBitmap(Context activityContext) {
+        //maybe later more things (like animations and so on)
+        //return BitmapFactory.decodeResource(activityContext.getResources(),
+          //      this.getImg()[this.getActiveDrawable()]);
+    }*/
 
 
     /** TODO
@@ -47,49 +63,24 @@ public abstract class Background {
      * After the first image is completely out of sight, the next one will appear (but of course, the fuck it doenst appear smoothly)
      *
      */
-    public abstract void updateBackground(int backgroundSpeed);
+    public abstract void updateBackground(); //if we want to change the speed just use the setter!
 
     //returns how many drawables for this background are defined
     public int getLengthOfBackground(){
         return getImg().length;
     }
 
-    public void setBackgroundSpeed(int speedX){
-        this.speedX = speedX;
+    public void setBackgroundSpeed(int backgroundSpeedX){
+        this.backgroundSpeedX = backgroundSpeedX;
     }
 
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x){
-        this.x=x;
-    }
-
-    public void setActiveDrawable(int pos){
-        activeDrawable = pos;
-    }
-
-    public int getActiveDrawable(){
-        return activeDrawable;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public int getSpeedX() {
-        return speedX;
+    public int getBackgroundSpeedX() {
+        return backgroundSpeedX;
     }
 
     public int getDisplay(int pos){
         return getImg()[pos];
     }
-
 
     public int[] getImg() {
         return img;
@@ -107,11 +98,26 @@ public abstract class Background {
         this.name = name;
     }
 
+    /**Dummy method */
     public GameView getGameView() {
-        return gameView;
+        return this.getBackgroundManager().getGameView();
     }
 
-    public void setGameView(GameView gameView) {
-        this.gameView = gameView;
+    /** image from the int array which is visible*/
+    public BackgroundManager getBackgroundManager() {
+        return backgroundManager;
     }
+
+    public void setBackgroundManager(BackgroundManager backgroundManager) {
+        this.backgroundManager = backgroundManager;
+    }
+
+    public Random getRandom() {
+        return random;
+    }
+
+    public void setRandom(Random random) {
+        this.random = random;
+    }
+
 }
