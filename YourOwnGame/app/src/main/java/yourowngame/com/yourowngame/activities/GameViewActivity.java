de37efb0eeb6223df5c4f38f5a32943e853738d8
Solution49 <crisjonny@live.at>
2018-02-23 17:53:53 +0100

    clouds movement, please change number of clouds, time to spawn etc.

--- YourOwnGame/app/src/main/java/yourowngame/com/yourowngame/activities/GameViewActivity.java
+++ YourOwnGame/app/src/main/java/yourowngame/com/yourowngame/activities/GameViewActivity.java
@@ -1,9 +1,13 @@
 package yourowngame.com.yourowngame.activities;
 
 import android.graphics.Bitmap;
+import android.hardware.display.DisplayManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
+import android.webkit.WebIconDatabase;
 import android.widget.FrameLayout;
 
 import yourowngame.com.yourowngame.R;
@@ -23,6 +27,8 @@ public class GameViewActivity extends AppCompatActivity {
     private FrameLayout gameLayout;
     private Player playerOne;
     private Bitmap map; //just for testing purpose
+    public static int GAME_HEIGHT;
+    public static int GAME_WIDTH;
 
     private boolean isTouched = false; //Screen is touched, player will move (DrawablePanel manages this)
 
@@ -32,15 +38,27 @@ public class GameViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_view);
 
+        getGameDimens();
+
         /*If we want to hide actionbar: (but I think we could add pause button etc. in actionbar)
         if (getSupportActionBar() == null) {Log.i(TAG, "Actionbar already gone/hidden.");} else {getSupportActionBar().hide();}*/
 
         Log.d(TAG, "onCreate: Trying to load game.");
         setGameLayout((FrameLayout) findViewById(R.id.gameViewLayout));
 
+
         /** Master-call, create GameView*/
         getGameLayout().addView(new GameView(this));
 
+    }
+
+    //Gets the current dimens, and saves it into STATIC Values, so we dont need to f* pass the activity onto the darkest point of our prog
+    private void getGameDimens(){
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        GAME_WIDTH = displayMetrics.widthPixels;
+        GAME_HEIGHT = displayMetrics.heightPixels;
+        Log.d(TAG, "HEIGHT = " + GAME_HEIGHT + "WIDTH = " + GAME_WIDTH);
     }
 
     //GETTER/SETTER (Base class)
@@ -71,6 +89,8 @@ public class GameViewActivity extends AppCompatActivity {
     public int getHeight(){
         return gameLayout.getHeight();
     }
+
+
 }
 
 
