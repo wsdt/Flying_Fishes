6e6da3f9eab28d6bf1af6ffff41dae6f7e6b16d0
Solution49 <crisjonny@live.at>
2018-02-27 15:48:23 +0100

    Just a Projectile.class, morgen weiter!

---
+++ YourOwnGame/app/src/main/java/yourowngame/com/yourowngame/classes/actors/Projectile.java
@@ -0,0 +1,40 @@
+package yourowngame.com.yourowngame.classes.actors;
+
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Solution on 27.02.2018.
+ *
+ *  Projectiles which the player can shoot to kill enemys!
+ *  Later on, projectiles will be available to buy in the shop (f.e)
+ *
+ *  Projectiles will be used as Singletone
+ *
+ *  Everytime the player hits a button or something else, projectiles will be shown
+ *
+ *  So shall we create a Singletone with a List of Projectiles?
+ */
+
+public class Projectile extends GameObject {
+
+    public Projectile(double posX, double posY, double speedX, double speedY, int[] img, float rotationDegree, @Nullable String name) {
+        super(posX, posY, speedX, speedY, img, rotationDegree, name);
+    }
+
+
+    @Override
+    public void update(GameObject obj, @Nullable Boolean goUp, @Nullable Boolean goForward) {
+
+    }
+
+    @Override
+    public boolean collision(View view, GameObject obj) {
+        return false;
+    }
+
+
+}
