c1185ed05bd2fa8602a4e8d000ccc9c9ac0330fa
Solution49 <crisjonny@live.at>
2018-03-08 01:06:23 +0100

    bug is still active, need to sleep now, see changelog

--- YourOwnGame/app/src/main/java/yourowngame/com/yourowngame/classes/actors/Enemy.java
+++ YourOwnGame/app/src/main/java/yourowngame/com/yourowngame/classes/actors/Enemy.java
@@ -1,7 +1,9 @@
 package yourowngame.com.yourowngame.classes.actors;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.support.annotation.NonNull;
@@ -21,15 +23,18 @@ import yourowngame.com.yourowngame.classes.exceptions.NoDrawableInArrayFound_Exc
 import yourowngame.com.yourowngame.classes.handler.RandomHandler;
 import yourowngame.com.yourowngame.gameEngine.GameView;
 
-/**
- * during development rich value methods should be on Top
- * draw()
- * update()
- * hitsTheGround()
- * aimToPlayer()
+/** PLEASE READ
  *
+ * So basically we're doing the following now (based on whatsapp)
  *
+ * We're creating a Singleton-Enemy.class, which provides Enemies for each level
+ * The Enemies are getting created in the @createRandomEnemies() method.
  *
+ * If other classes need a reference on that, just use
+ *
+ * getInstance().getEnemys()
+ *
+ * that will always return the enemys for the current level.
  *
  */
 
@@ -37,7 +42,7 @@ import yourowngame.com.yourowngame.gameEngine.GameView;
 public class Enemy extends GameObject {
     private static final String TAG = "Enemy";
     private static Enemy INSTANCE;
-    private static List<Enemy> enemyList = new ArrayList<>();
+    private List<Enemy> enemyList = new ArrayList<>(); //do not make that static, already Singleton class
     private Player player;
 
     // PRE LOADED ---------------
@@ -58,12 +63,103 @@ public class Enemy extends GameObject {
     public void update(GameObject obj, @Nullable Boolean goUp, @Nullable Boolean goForward) {}
 
 
-    // same bug here..
     @Override
     public void draw(@NonNull Activity activity, @NonNull Canvas canvas, long loopCount) throws NoDrawableInArrayFound_Exception {
-//       this.setCurrentBitmap(this.getLoadedBitmaps().get(this.getRotationDegree()+"_"+((int) loopCount % this.getImg().length)));
-//        canvas.drawBitmap(this.getCurrentBitmap(),(int) this.getPosX(), (int) this.getPosY(), null);
+        for (Enemy enemy : getEnemys()) {
+            //set the bitmap, here is the bug, getLoadedBitmaps() returns null, need to sleep now, im done haha
+            //does also not belong here!
+            enemy.setCurrentBitmap(enemy.getLoadedBitmaps().get(enemy.getRotationDegree() + "_" + ((int) loopCount % enemy.getImg().length)));
+            Log.d(TAG, "Enemy getBitmap = " + enemy.getLoadedBitmaps().get(enemy.getRotationDegree() + "_" + ((int) loopCount % enemy.getImg().length)));
+            //draw the enemy
+            canvas.drawBitmap(enemy.getCurrentBitmap(), (int) enemy.getPosX(), (int) enemy.getPosY(), null);
+        }
     }
+
+
+    @Deprecated //do not use
+    public void drawAllEnemies(@NonNull Activity activity, @NonNull Canvas canvas, long loopCount) throws NoDrawableInArrayFound_Exception {
+        for (Enemy enemy : getEnemys()) {
+            enemy.draw(activity, canvas, loopCount);
+        }
+        if (getEnemys().size() <= 0) { //i am sure this will happen to us ^^
+            Log.w(TAG, "drawAllEnemies: No enemies found. Maybe you did not call createRandomEnemies()!");
+        }
+    }
+
+
+    //TODO: Current design says this method is called in init() of GameView, but it would be in future maybe more object-oriented if we do that in initialize() below
+    //TODO: i would say the method should be called separately.. but let's flip for it! :D
+    public void createRandomEnemies(int numberOfEnemys, int[] img) {
+        for (int i = 0; i <= numberOfEnemys; i++) {
+            getInstance().enemyList.add(new Enemy(
+                    RandomHandler.getRandomInt(GameViewActivity.GAME_WIDTH, GameViewActivity.GAME_WIDTH + 200),
+                    RandomHandler.getRandomInt(GameViewActivity.GAME_HEIGHT / 2, GameViewActivity.GAME_HEIGHT),
+                    RandomHandler.getRandomFloat(Constants.Actors.Enemy.speedXmin, Constants.Actors.Enemy.speedXmax),
+                    RandomHandler.getRandomFloat(Constants.Actors.Enemy.speedYmin, Constants.Actors.Enemy.speedYmax),
+                    img, Constants.Actors.Enemy.defaultRotation, "Enemy Nr._" + i+1));
+        }
+    }
+
+    /** OBJ[0]: Activity
+     *
+     * We currently do only set the image for each created Enemy, nothing more!
+     * Later on we could create the enemies here.. but lets get it working first
+     *
+     * WORKS NOW! Bug was in the getCraftedDynamicBitmap, which was all the time referencing our fucking getInstance, zum blean haha
+     *
+     * So we really need to optimize the craftBitmap methods, one here, the other one in the gameview.
+     * */
+    @Override @SafeVarargs
+    public final <OBJ> boolean initialize(@Nullable OBJ... allObjs) {
+        try {
+            if (allObjs != null) {
+                Log.d(TAG, "initialize: AllObj: "+allObjs[0]);
+                if (allObjs[0] instanceof Activity) {
+                    Log.d(TAG, "initialize: Starting with bitmap extraction.");
+                    Activity activity = (Activity) allObjs[0];
+
+                    /*Load all bitmaps [load all rotations and all images from array] -------------------
+                    * String of hashmap has following pattern: */
+
+                    loadedBitmaps = new HashMap<>();
+
+                    for(Enemy currentEnemy : getEnemys()) {
+                        Log.d(TAG, "enemy length = " + getEnemys().size());
+                        Log.d(TAG, "enemy img length = " + currentEnemy.getImg().length);
+                        for (int imgFrame = 0; imgFrame < currentEnemy.getImg().length; imgFrame++) {
+                                //Just create the f* bitmap and pass it onto the currentEnemy
+                                loadedBitmaps.put(Constants.Actors.Enemy.rotationFlyingUp + "_" + imgFrame, createBitmap(activity, currentEnemy, imgFrame));
+                                Log.d(TAG, "Created a bitmap! = " + loadedBitmaps.size());
+
+                                // NOT WORKING, @GetCraftedDynamicBitmap will not work here!
+                                // loadedBitmaps.put(Constants.Actors.Enemy.rotationFlyingUp + "_" + imgFrame, getCraftedDynamicBitmap(activity, e.getImg()[imgFrame], Constants.Actors.Enemy.rotationFlyingUp, Constants.Actors.Enemy.widthPercentage, Constants.Actors.Enemy.heightPercentage));
+                                // loadedBitmaps.put(Constants.Actors.Enemy.rotationFlyingDown + "_" + imgFrame, getCraftedDynamicBitmap(activity, e.getImg()[imgFrame], Constants.Actors.Enemy.rotationFlyingUp, Constants.Actors.Enemy.widthPercentage, Constants.Actors.Enemy.heightPercentage));
+                                // Log.d(TAG, "Bitmap added" + loadedBitmaps.size());
+                        }
+                        currentEnemy.setLoadedBitmaps(loadedBitmaps);
+                    }
+                }
+            } else {return false;}
+        } catch (ClassCastException | NullPointerException e) {
+            //This should never be thrown! Just check in try block if null and if instance of to prevent issues!
+            Log.e(TAG, "initialize: Initializing of Enemy object FAILED! See error below.");
+            e.printStackTrace();
+            return false;
+        }
+        Log.d(TAG, "initialize Enemy: SUCCESS");
+
+        return true;
+    }
+
+    @Override
+    public boolean cleanup() {
+        this.setLoadedBitmaps(null);
+        return true;
+    }
+
+    /***********************
+     *    AI & such stuff  *
+     ***********************/
 
     public void aimToPlayer(Player player) {
         this.player = player; //not really necess
@@ -82,86 +178,22 @@ public class Enemy extends GameObject {
         }
     }
 
+    /** Class intern method for creating bitmaps, getCr...Bitmap wont work, because of this-reference
+     * i just implemented a just-get-a-fucking-img-method, because we really need to enhance our current
+     * getCraftedDynamicDrawable-method, neither did I exception handling*/
+    public Bitmap createBitmap(Activity context, Enemy e, int imgFrame){
+        return BitmapFactory.decodeResource(context.getResources(), e.getImg()[imgFrame]);
+    }
+
+    /************************
+     *   GETTER & SETTER    *
+     ************************/
+
     //Returns the enemyList, ready for drawing, rendering etc.
     public List<Enemy> getEnemys(){
         return enemyList;
     }
 
-    public void drawAllEnemies(@NonNull Activity activity, @NonNull Canvas canvas, long loopCount) throws NoDrawableInArrayFound_Exception {
-        for (Enemy enemy : getEnemys()) {
-            enemy.draw(activity, canvas, loopCount);
-        }
-        if (getEnemys().size() <= 0) { //i am sure this will happen to us ^^
-            Log.w(TAG, "drawAllEnemies: No enemies found. Maybe you did not call createRandomEnemies()!");
-        }
-    }
-
-    //So we have all parameters we need quite compact in the GameView.class (by creation)
-    //TODO: Current design says this method is called in init() of GameView, but it would be in future maybe more object-oriented if we do that in initialize() below
-    public void createRandomEnemies(GameView gameView, int numberOfEnemys, int[] img, @Nullable String name) {
-        //add enemy to list, numberOfEnemys-1 or the the enemy draws itself too!
-        for (int i = 0; i <= numberOfEnemys; i++) {
-            Enemy.enemyList.add(new Enemy(
-                    // so 200 divided by the speed equals the amount of time, in which the enemys will spawn!
-                    RandomHandler.getRandomInt(GameViewActivity.GAME_WIDTH, GameViewActivity.GAME_WIDTH + 200),
-                    RandomHandler.getRandomInt(GameViewActivity.GAME_HEIGHT / 2, GameViewActivity.GAME_HEIGHT),
-                    RandomHandler.getRandomFloat(Constants.Actors.Enemy.speedXmin, Constants.Actors.Enemy.speedXmax),
-                    RandomHandler.getRandomFloat(Constants.Actors.Enemy.speedYmin, Constants.Actors.Enemy.speedYmax),
-                    img, Constants.Actors.Enemy.defaultRotation, name));
-
-            //Enemy.enemyList.get(i).setImg(img);
-        }
-    }
-
-
-    /** TODO what happens here really, we need to set the bitmaps for every Enemy e! this. -> wont work here, we've got no this, every Enemey e needs its own drawables, somehow...  */
-
-    /** OBJ[0]: Activity */
-    @Override @SafeVarargs
-    public final <OBJ> boolean initialize(@Nullable OBJ... allObjs) {
-        try {
-            if (allObjs != null) {
-                Log.d(TAG, "initialize: AllObj: "+allObjs[0]);
-                if (allObjs[0] instanceof Activity) {
-                    Log.d(TAG, "initialize: Starting with bitmap extraction.");
-                    Activity activity = (Activity) allObjs[0];
-
-                    /*Load all bitmaps [load all rotations and all images from array] -------------------
-                    * String of hashmap has following pattern: */
-
-                    HashMap<String, Bitmap> loadedBitmaps = new HashMap<>();
-
-                    for(Enemy e : getEnemys()) {
-                        Log.d(TAG, "enemy length = " + getEnemys().size());
-                        for (int imgFrame = 0; imgFrame < e.getImg().length; imgFrame++) {
-
-                                loadedBitmaps.put(Constants.Actors.Enemy.rotationFlyingUp + "_" + imgFrame, getCraftedDynamicBitmap(activity, e.getImg()[imgFrame], Constants.Actors.Enemy.rotationFlyingUp, Constants.Actors.Enemy.widthPercentage, Constants.Actors.Enemy.heightPercentage));
-                                loadedBitmaps.put(Constants.Actors.Enemy.rotationFlyingDown + "_" + imgFrame, getCraftedDynamicBitmap(activity, e.getImg()[imgFrame], Constants.Actors.Enemy.rotationFlyingUp, Constants.Actors.Enemy.widthPercentage, Constants.Actors.Enemy.heightPercentage));
-                                Log.d(TAG, "Bitmap added" + loadedBitmaps.size());
-                        }
-                        e.setLoadedBitmaps(loadedBitmaps);
-                    }
-
-                }
-            } else {return false;}
-        } catch (ClassCastException | NullPointerException | NoDrawableInArrayFound_Exception e) {
-            //This should never be thrown! Just check in try block if null and if instance of to prevent issues!
-            Log.e(TAG, "initialize: Initializing of Enemy object FAILED! See error below.");
-            e.printStackTrace();
-            return false;
-        }
-        Log.d(TAG, "initialize: Tried to initialize Enemy object!");
-
-        return true;
-    }
-
-    @Override
-    public boolean cleanup() {
-        this.setLoadedBitmaps(null);
-        return true;
-    }
-
-    //GETTER/SETTER -----------------------------------
     public HashMap<String, Bitmap> getLoadedBitmaps() {
         return loadedBitmaps;
     }
