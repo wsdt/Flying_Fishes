da5b577ad682030f7f3693393a4553870a0d3c35
kevin <kevin.riedl.privat@gmail.com>
2018-04-26 13:01:57 +0200

    annotations added

---
+++ YourOwnGame/app/src/main/java/yourowngame/com/yourowngame/classes/annotations/Enhance.java
@@ -0,0 +1,29 @@
+package yourowngame.com.yourowngame.classes.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+/** This annotation should be used to indicate that the method, class or other members should be
+ * improved e.g. by:
+ *      - Performance
+ *      - Concise/Legible Code
+ *      - Additional features
+ *      - Solving multiple bugs (single bugs might be just marked with an TO´DO comment. */
+
+@Target({ElementType.METHOD,ElementType.CONSTRUCTOR,
+        ElementType.FIELD,ElementType.PARAMETER,
+        ElementType.LOCAL_VARIABLE,ElementType.TYPE,ElementType.ANNOTATION_TYPE,ElementType.PACKAGE})
+@Retention(RetentionPolicy.CLASS)
+public @interface Enhance {
+    enum Priority {
+        NOT_DETERMINED, LOW, MEDIUM, HIGH
+    }
+
+    //Default params
+    Priority[] priority() default Priority.NOT_DETERMINED;
+    String[] message() default "No message provided";
+    String[] byDeveloper() default "No developer specified.";
+}
---
+++ YourOwnGame/app/src/main/java/yourowngame/com/yourowngame/classes/annotations/Test.java
@@ -0,0 +1,25 @@
+package yourowngame.com.yourowngame.classes.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+/** This annotation should be used to indicate that the method, variable, class etc. should
+ * be tested carefully. */
+
+@Target({ElementType.METHOD,ElementType.CONSTRUCTOR,
+        ElementType.FIELD,ElementType.PARAMETER,
+        ElementType.LOCAL_VARIABLE,ElementType.TYPE})
+@Retention(RetentionPolicy.CLASS)
+public @interface Test {
+    enum Priority {
+        NOT_DETERMINED, LOW, MEDIUM, HIGH
+    }
+
+    //Default params (as array to allow multiple)
+    Priority[] priority() default Priority.NOT_DETERMINED;
+    String[] message() default "No message provided";
+    String[] byDeveloper() default "No developer specified.";
+}
