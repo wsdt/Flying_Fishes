2b1fbf013f925258496098cf0ee5597e17676868
kevin <kevin.riedl.privat@gmail.com>
2018-05-02 20:53:45 +0200

    i think have got a reference problem, we need to move it all to level obj (all enemies etc.)

---
+++ YourOwnGame/app/src/main/java/yourowngame/com/yourowngame/classes/gamelevels/levels/Level_LostPlace.java
@@ -0,0 +1,92 @@
+package yourowngame.com.yourowngame.classes.gamelevels.levels;
+
+import android.util.Log;
+
+import java.util.Arrays;
+
+import yourowngame.com.yourowngame.R;
+import yourowngame.com.yourowngame.classes.actors.enemy.Enemy;
+import yourowngame.com.yourowngame.classes.actors.enemy.specializations.BobaEnemy;
+import yourowngame.com.yourowngame.classes.actors.enemy.specializations.HappenEnemy;
+import yourowngame.com.yourowngame.classes.actors.enemy.specializations.RocketFishEnemy;
+import yourowngame.com.yourowngame.classes.actors.fruits.Fruit;
+import yourowngame.com.yourowngame.classes.actors.fruits.FruitMgr;
+import yourowngame.com.yourowngame.classes.actors.player.Player;
+import yourowngame.com.yourowngame.classes.actors.player.interfaces.IPlayer;
+import yourowngame.com.yourowngame.classes.background.Background;
+import yourowngame.com.yourowngame.classes.background.interfaces.IBackground;
+import yourowngame.com.yourowngame.classes.background.layers.BackgroundLayer_Clouds;
+import yourowngame.com.yourowngame.classes.background.layers.BackgroundLayer_staticBgImg;
+import yourowngame.com.yourowngame.classes.gamelevels.Level;
+import yourowngame.com.yourowngame.classes.gamelevels.LevelManager;
+
+
+/**
+ * LEVEL ONE
+ */
+
+
+public class Level_LostPlace extends Level implements IBackground {
+    private static final String TAG = "Lvl_HarmlessSky";
+
+    @Override
+    protected void determinePlayer() {
+        this.setPlayer(new Player(100, LevelManager.getBackgroundManager().getGameView().getRootView().getHeight() / 4, 5, 2, new int[]{
+                R.drawable.player_hugo}, IPlayer.DEFAULT_ROTATION, "Hugo"));
+    }
+
+    @Override
+    protected void determineBackgroundLayers() {
+        /*This.getAllBackgroundLayers can be directly used with add without additional declaration, because object is initialized implicitly
+        * - Add layers acc. to the desired order (first add() is the lowest layer etc.)*/
+        this.getAllBackgroundLayers().add(new BackgroundLayer_staticBgImg(LevelManager.getBackgroundManager(), R.color.colorPrimary, "Sky", DEFAULT_BG_SPEED));
+        this.getAllBackgroundLayers().add(new BackgroundLayer_Clouds(LevelManager.getBackgroundManager(), new int[]{R.drawable.bglayer_1_cloud_1,R.drawable.bglayer_1_cloud_2,R.drawable.bglayer_1_cloud_3}, "Heaven", DEFAULT_BG_SPEED));
+
+        Log.d(TAG, "determineBackgroundLayers: Have set layers.");
+        //no setAllBackgroundLayers necessary (reference)
+    }
+
+    @Override
+    protected void determineAllEnemies() { //Only exception (initialize() here instead of in obj constr, because of createRandomEnemies())
+        //Set allEnemies Arraylist
+
+        /**Initializing Rocket-Enemy */
+        RocketFishEnemy rocketEnemyManager = new RocketFishEnemy();
+        rocketEnemyManager.createRandomEnemies(8);
+
+        this.getAllEnemies().addAll(RocketFishEnemy.getEnemyList());
+        Log.d(TAG, "determineAllEnemies: Have set global level-dependent enemylist.");
+    }
+
+    @Override
+    protected void determineFruits() {
+        /****************************
+         *  FRUIT INITIALIZING AREA *
+         ****************************/
+
+        this.getAllFruits().addAll(Arrays.asList(FruitMgr.createMeloon(), FruitMgr.createMeloon()));
+    }
+
+    @Override
+    public void cleanUpLevelProperties() {
+        //CleanUp Player
+        this.getPlayer().cleanup();
+
+        //CleanUp Enemies
+        for (Enemy enemy : this.getAllEnemies()) {
+            enemy.cleanup();
+        }
+
+        //CleanUp Bglayers
+        for (Background background : this.getAllBackgroundLayers()) {
+            background.cleanup();
+        }
+
+        Log.d(TAG, "cleanUpLevelProperties: Clean up all level properties.");
+    }
+
+    @Override
+    protected void playBackgroundMusic() {
+        //TODO: play bg sound [search resource] --> Level_HarmlessSky.soundMgr.play(LevelManager.getBackgroundManager().getGameView().getActivityContext(),R.raw.bgMusicLvl1,true);
+    }
+}
