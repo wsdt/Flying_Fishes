8ab872b5171cae3acd5dd8785b19e238bb71bd87
kevin <kevin.riedl.privat@gmail.com>
2018-05-09 17:33:43 +0200

    added levelAssigment.class

---
+++ YourOwnGame/app/src/main/java/yourowngame/com/yourowngame/classes/gamelevels/levelassignments/LA_AchievePoints.java
@@ -0,0 +1,32 @@
+package yourowngame.com.yourowngame.classes.gamelevels.levelassignments;
+
+import android.support.annotation.NonNull;
+
+import yourowngame.com.yourowngame.R;
+import yourowngame.com.yourowngame.classes.counters.Highscore;
+import yourowngame.com.yourowngame.classes.gamelevels.LevelAssignment;
+
+public class LA_AchievePoints extends LevelAssignment {
+    private Highscore highscore;
+
+    public LA_AchievePoints(long amount, @NonNull Highscore highscore) {
+        this.setAmount(amount);
+        this.setResDrawable_smallIcon(R.drawable.app_icon_gameboy); //todo: change
+        this.setResString_whatToDo(R.string.levelAssignments_achievePoints_whatToDo);
+        this.setHighscore(highscore);
+    }
+
+    @Override
+    public boolean isLevelAssignmentAchieved() {
+        return (getHighscore().getValue() >= getAmount());
+    }
+
+    //GETTER/SETTER ------------------------------------------------------------
+    public Highscore getHighscore() {
+        return highscore;
+    }
+
+    public void setHighscore(Highscore highscore) {
+        this.highscore = highscore;
+    }
+}
