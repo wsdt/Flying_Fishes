e22b74238a775e7ee71dafa651fc2e918c6efb0f
kevin <kevin.riedl.privat@gmail.com>
2018-06-26 15:24:19 +0200

    lines etc.

--- YourOwnGame/app/src/main/java/yourowngame/com/yourowngame/gameEngine/surfaces/WorldView.java
+++ YourOwnGame/app/src/main/java/yourowngame/com/yourowngame/gameEngine/surfaces/WorldView.java
@@ -1,23 +1,29 @@
 package yourowngame.com.yourowngame.gameEngine.surfaces;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
+import android.graphics.Rect;
 import android.support.annotation.NonNull;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 
 import java.util.Map;
 
 import yourowngame.com.yourowngame.R;
+import yourowngame.com.yourowngame.activities.GameViewActivity;
 import yourowngame.com.yourowngame.activities.WorldActivity;
 import yourowngame.com.yourowngame.classes.annotations.Testing;
 import yourowngame.com.yourowngame.classes.background.Background;
 import yourowngame.com.yourowngame.classes.gamedesign.Level;
+import yourowngame.com.yourowngame.classes.gamedesign.LevelManager;
 import yourowngame.com.yourowngame.classes.gamedesign.World;
+import yourowngame.com.yourowngame.classes.gamedesign.levels.Level_NightRider;
 import yourowngame.com.yourowngame.classes.gamedesign.worlds.World_Earth;
 import yourowngame.com.yourowngame.gameEngine.DrawableSurfaces;
 
@@ -29,6 +35,7 @@ public class WorldView extends DrawableSurfaces {
 
     private World currWorld;
     private Bitmap initializedLevelRepresentant;
+    private LevelManager levelManager;
 
     public WorldView(Context context) {
         super(context);
@@ -46,6 +53,7 @@ public class WorldView extends DrawableSurfaces {
         this.setDrawableSurfaceActivity(worldActivity);
         //TODO: Just for testing
         this.setCurrWorld(new World_Earth(worldActivity));
+        this.levelManager = new LevelManager(worldActivity);
 
         this.initializeDrawableObjs();
         this.initializeDrawing();
@@ -91,10 +99,15 @@ public class WorldView extends DrawableSurfaces {
             /* Draw levels */
             Log.d(TAG, "drawAll: Trying to draw levelRepresentants. Count: "+this.getCurrWorld().getAllLevels().size());
             Point positionOfLastLevel = null; // to draw line
+            // Calculate how much we need to add so the lines are in the mid of the icons.
+            float addToX = this.getInitializedLevelRepresentant().getWidth()/2;
+            float addToY = this.getInitializedLevelRepresentant().getHeight()/2;
             for (Map.Entry<Point,Level> level : this.getCurrWorld().getAllLevels().entrySet()) {
+                //Draw connection lines
                 if (positionOfLastLevel != null) {
-                    canvas.drawLine(positionOfLastLevel.x,positionOfLastLevel.y,level.getKey().x,level.getKey().y, new Paint(R.color.colorBlack));
+                    canvas.drawLine(addToX+positionOfLastLevel.x,addToY+positionOfLastLevel.y,addToX+level.getKey().x,addToY+level.getKey().y, new Paint(R.color.colorBlack));
                 }
+                
                 canvas.drawBitmap(this.getInitializedLevelRepresentant(),level.getKey().x,level.getKey().y,null);
                 positionOfLastLevel = level.getKey();
 
